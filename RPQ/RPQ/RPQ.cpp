8b3058cd859c6f5f67b39a2811214d432228921b
Bialy <mat327@interia.pl>
2020-04-04 17:00:28 +0200

    WiTi Schrange RPQ

---
+++ RPQ/RPQ/RPQ.cpp
@@ -0,0 +1,164 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <stdexcept>
+#include <algorithm>
+#include <string>
+#include <chrono>
+
+using namespace std;
+
+vector<int> R, P, Q;
+string line;
+int quantityOfDataLines = 0;
+int quantityofElements = 0;
+ifstream myfile("data.txt");
+
+
+void printVector(const vector<int> VEC);
+bool isVectorContainingOnlyBigNumbers(const vector<int> VEC, const int BIG_NUMBER);
+vector<int> getNewOrderWithSortR(vector<int> localR);
+vector<int> getReorganisedVector(const vector<int> VEC, const vector<int> NEW_ORDER);
+void printVectorWithIncrementedValues(vector<int> VEC);
+int calculateC_MAX(vector<int> localR, vector<int> localP, vector<int> localQ);
+
+int main() {
+    string line;
+    string dataName;
+    int tempR, tempP, tempQ;
+    vector<int> newOrder;
+
+    vector<int> newR;
+    vector<int> newP;
+    vector<int> newQ;
+
+    vector<int> Cmax;
+
+    if (myfile.is_open()) {
+
+        int tab[6] = { 10, 20, 50, 100, 200, 500};
+        for (int i = 0; i < 6; i++) {
+            dataName = "data" + to_string(tab[i]);
+
+            // Skip useless data
+            while (getline(myfile, line) && line != dataName) {}
+
+            // read quantity of measures
+            myfile >> quantityOfDataLines >> quantityofElements;
+
+            // read data
+
+            for (int i = 0; i < quantityOfDataLines; i++) {
+                myfile >> tempR >> tempP >> tempQ;
+                R.push_back(tempR);
+                P.push_back(tempP);
+                Q.push_back(tempQ);
+            }
+
+
+            cout << "CMAX for initialorder = " << calculateC_MAX(R, P, Q) << endl;
+
+
+            auto start = chrono::steady_clock::now();
+
+            vector<int> newOrder = getNewOrderWithSortR(R);
+
+            vector<int> newR = getReorganisedVector(R, newOrder);
+            vector<int> newP = getReorganisedVector(P, newOrder);
+            vector<int> newQ = getReorganisedVector(Q, newOrder);
+
+           
+            Cmax.push_back(calculateC_MAX(newR, newP, newQ));
+
+
+           // cout << "New order for " + dataName + " with SortR "; printVector(newOrder);
+            cout << "Calculated Cmax for " + dataName + " = " << Cmax[i] << "\n\n"; // has to be i-1 cuz we got date from 1 to 4 while Cmax start indexing from zero
+
+            //clear vectors
+            R.clear(); P.clear(); Q.clear();
+            newOrder.clear(); newR.clear(); newP.clear(); newQ.clear();
+
+        }
+        myfile.close();
+
+        cout << "Sum for every Cmax = " << Cmax[0] + Cmax[1] + Cmax[2] + Cmax[3] << "\n";
+
+    }
+    else {
+        cerr << "Unable to open file. Exit program.\n";
+    }
+    return 0;
+}
+
+
+
+
+vector<int> getNewOrderWithSortR(vector<int> localR) {
+    const int BIG_NUMBER = 1999999999;
+
+    vector<int> newOrder;
+    int smallestNumber;
+    int smallestNumberIndex;
+
+    while (isVectorContainingOnlyBigNumbers(localR, BIG_NUMBER) == false) {
+
+        smallestNumber = localR[0];
+        smallestNumberIndex = 0;
+
+        // find index of the smallest number in vector
+        for (int i = 0; i < localR.size(); i++) {
+            if (smallestNumber > localR[i]) {
+
+                smallestNumber = localR[i];
+                smallestNumberIndex = i;
+            }
+        }
+        newOrder.push_back(smallestNumberIndex);
+        localR[smallestNumberIndex] = BIG_NUMBER; // use assignment instead of erase!! cuz with erase() you modifying initial order!!
+    }
+    return newOrder;
+}
+bool isVectorContainingOnlyBigNumbers(const vector<int> VEC, const int BIG_NUMBER) {
+    for (int i = 0; i < VEC.size(); i++) {
+        if (VEC[i] != BIG_NUMBER) return false;
+    }
+    return true;
+}
+
+vector<int> getReorganisedVector(const vector<int> VEC, const vector<int> NEW_ORDER) {
+    if (VEC.size() != NEW_ORDER.size()) {
+        throw invalid_argument("Invalid arguments! Vectors sizes are diffrent. can not get reorder vector.\n");
+    }
+
+    vector<int> reOrderedVector;
+    int indexOfNextSmallestNumber;
+
+    for (int i = 0; i < NEW_ORDER.size(); i++) {
+        indexOfNextSmallestNumber = NEW_ORDER[i];
+        reOrderedVector.push_back(VEC[indexOfNextSmallestNumber]);
+    }
+    return reOrderedVector;
+}
+
+void printVector(const vector<int> VEC) {
+    cout << " [";
+    for (int i = 0; i < VEC.size(); i++) {
+        cout << " " << VEC[i];
+    }
+    cout << " ]\n";
+}
+
+
+
+int calculateC_MAX(vector<int> localR, vector<int> localP,
+    vector<int> localQ) {
+
+
+    int m = 0, c = 0;
+    for (int i = 0; i < localR.size(); i++) {
+        m = max(localR[i], m) + localP[i];
+        c = max(c, m + localQ[i]);
+    }
+
+    return c;
+}
---
+++ Schrange/Schrange/Schrange.cpp
@@ -0,0 +1,202 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <limits>
+#include <chrono>
+
+using namespace std;
+
+class Job
+{
+public:
+    int R;
+    int P;
+    int Q;
+    int initialP;
+
+    int initialIndex;
+
+    Job job() {};
+
+    void showJob() {
+        std::cout << "R: " << R << " P: " << P << " Q: " << Q << " init index: " << initialIndex + 1 << "\n";
+    }
+};
+
+int calculateC_MAX(std::vector<Job>& jobs) {
+    int m = 0, c = 0;
+    for (int i = 0; i < jobs.size(); i++) {
+        m = std::max(jobs[i].R, m) + jobs[i].P;
+        c = std::max(c, m + jobs[i].Q);
+    }
+    return c;
+}
+
+
+/*
+ Funkcja znajduje indexy dostępnych zadań w danym przedziale czasu
+
+ do tej fuunkcji trzeba przekazać vector prac z usunietymi pracami które zostały wykonane
+*/
+
+std::vector<int> getIndiciesAvailableJobs(std::vector<Job>& jobs, int elapsedTime) {
+    std::vector<int> result;
+    for (int i = 0; i < jobs.size(); i++) {
+        if (jobs[i].R <= elapsedTime) {
+            result.push_back(i);
+        }
+    }
+
+    return result;
+}
+
+// Funkcja dodaje zadanie o największym Q do vectora result
+// zwraca kolejność uszeregowania
+std::vector<Job>& addNewJobWithBiggestQ(std::vector<Job>& jobs, std::vector<int>& availbleTasksIndices,
+    std::vector<Job>& resultJobs, int& elapsedTime) {
+
+    int biggestQIndex = availbleTasksIndices[0];
+    int biggestQ = jobs[biggestQIndex].Q;
+
+    for (int index : availbleTasksIndices) {
+        if (biggestQ < jobs[index].Q) {
+            biggestQ = jobs[index].Q;
+            biggestQIndex = index;
+
+        }
+        else if (biggestQ == jobs[index].Q) { // jeżeli takie same Q to bierzemy o mniejszym indexie początkowym
+
+            if (jobs[biggestQIndex].initialIndex > jobs[index].initialIndex) {
+                biggestQ = jobs[index].Q;
+                biggestQIndex = index;
+            }
+        }
+    }
+
+
+
+    resultJobs.push_back(jobs[biggestQIndex]);
+    elapsedTime += jobs[biggestQIndex].P;
+
+
+    jobs[biggestQIndex].R = std::numeric_limits<int>::max(); // podmieniamy wartość R zadania które własnie dodaliśmy do kolekcji result.
+
+    return resultJobs;
+}
+
+int main(int argc, char** argv) {
+
+    std::vector<Job> jobs, result;
+    Job temporaryJob;
+
+
+
+    std::ifstream myfile("data.txt");
+    int quantityOfData = 0;
+    int quantityOfElements = 0;
+
+    std::string line;
+
+
+    if (!myfile.is_open()) throw std::runtime_error("Could not open file");
+
+    std::string currentDataSet;
+
+    int tab[6] = { 10, 20, 50, 100, 200, 500 };
+
+    for (int dataSetIndex = 0; dataSetIndex < 6; dataSetIndex++) {
+
+        currentDataSet = "data" + std::to_string(tab[dataSetIndex]);
+
+        while (getline(myfile, line) && (line != currentDataSet)); // skip useless data
+        myfile >> quantityOfData >> quantityOfElements;
+        try
+        {
+            if (quantityOfData == 0) throw std::runtime_error(std::string("Could not read data from file. (probably inappropriate end of line character in variable currentDataSet)\n") +
+                "Try \\n, \\n\\r, \\r, or none");
+        }
+        catch (const std::exception& e)
+        {
+            std::cerr << e.what() << "\n";
+            exit(-1);
+        }
+
+        // read data
+
+        for (int i = 0; i < quantityOfData; i++) {
+            myfile >> temporaryJob.R >> temporaryJob.P >> temporaryJob.Q;
+            temporaryJob.initialIndex = i;
+
+            jobs.push_back(temporaryJob);
+        }
+
+        // Posortujmye vector prac po R rosnąco
+        auto start = chrono::steady_clock::now();
+        std::sort(jobs.begin(), jobs.end(), [](const Job& first, const Job& second) { return first.R < second.R; });
+
+
+        // Pierwsze dostępne zadanie będzie zadaniem o najmniejszym R
+        // Zadanie to będzie dostępne w momencie elapsedTime równym jego R
+        Job jobWithSmallestR = jobs[0];
+        int elapsedTime = jobWithSmallestR.R;
+
+        int initialQuantitiOfJobs = jobs.size();
+        std::vector<int> indicesAvailableJobs;
+
+        // Shrange zwykłe
+
+        // wykonujemy dopóki w kolekcji result jest mniej prac niż prac wczytanych
+
+        while (result.size() != initialQuantitiOfJobs) {
+
+            // Z puli zadań wyszukujemy te indexy zadań których R jest mniejsze bądź równe elapsedTime
+            // oraz nie bierzemy pod uwage zadań uprzednio dodanych do kolekcji result
+
+            indicesAvailableJobs = getIndiciesAvailableJobs(jobs, elapsedTime);
+
+            // W przypadku gdy obecnie nie ma żdanych dostępnych zadań w czasie elapsedTime
+            // To w kolekcji z zadaniami wyszukujemy pierwsze zadanie którego R jest większe od elapsedTime oraz różne od MAX_INT (ponieważ po dodaniu zadania do result, jego wartość R podmieniamy na MAX_INT)
+
+            if (indicesAvailableJobs.size() == 0) {
+                // W tym przypadku znaleźć pierwsze większe R niz elapsedTime z vectora jobs i przypisac je do elapsed time
+                for (int i = 0; i < jobs.size(); i++) {
+                    if (jobs[i].R > elapsedTime && (jobs[i].R != std::numeric_limits<int>::max())) {
+                        elapsedTime = jobs[i].R;
+                        // kiedy znaleźliśmy następne elapsedTime dla którego będą jakieś zadania, to ponownie wyszukujemy dostępne zadania
+                        indicesAvailableJobs = getIndiciesAvailableJobs(jobs, elapsedTime);
+                        break;
+                    }
+                }
+
+            }
+
+            // z puli dostępnych zadań, dodaj zadanie do kolekcji result na podstawie parametru Q
+            // w tej funkcji dzieje sie wiele rzeczy.
+            // dodajemy zadanie o najwiekszym Q do kolekcji result
+            // zwiększamy elapsed time o wartość P zadania które dodaliśmy do kolekcji result
+            // podmieniamy wartość R zadania na MAX_INT aby pomijać to zadanie w kolejnych iteracjach.
+            result = addNewJobWithBiggestQ(jobs, indicesAvailableJobs, result, elapsedTime);
+
+        }
+
+
+        int C_max = calculateC_MAX(result);
+
+        auto end = chrono::steady_clock::now();
+
+        cout << "Elapsed time in microseconds : "
+            << chrono::duration_cast<chrono::microseconds>(end - start).count()
+            << " µs" << endl;
+
+        std::cout << "\n Current data set:" << dataSetIndex << " C_max = " << C_max << "\n";
+        std::cout << "\n";
+
+        result.clear();
+        jobs.clear();
+    }
+
+
+    return 0;
+}
